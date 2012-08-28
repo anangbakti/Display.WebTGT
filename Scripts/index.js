4196a7a5b62d7e900c0344e9704de740d2e7933f
anangbakti <anang.bakti@gmail.com>
2012-08-28 09:50:42 +0700

    finish hiding header

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,20 +1,28 @@
 ﻿/// <reference path="jquery-1.8.0.min.js"/>
 /// <reference path="css-tools.js"/>
-var footerOpened, cssContentBoxNormal;
+var footerOpened, cssContentBoxNormal, cssNailHeaderNormal, cssLogoNormal, cssMenuBoxNormal;
 
-var cssContentBoxTopHidden= {'top': '0px'}
+var cssContentBoxTopHidden = { 'top': '0px' }
+var cssZeroWidth = { 'width': '0px' }
 
 $(document).ready(function () {
     footerOpened = true;
     cssContentBoxNormal = css($("#contentBox"));
+    cssNailHeaderNormal = css($("#nailHeader"));
+    cssLogoNormal = css($("#logo"));
+    cssMenuBoxNormal = css($("#menuBox"));
 
     $("#nailHeader").click(function () {
         if (footerOpened) {
-            $("#contentBox").css(cssContentBoxTopHidden);
+            $("#contentBox").css(cssContentBoxTopHidden);$("#nailHeader").css(cssContentBoxTopHidden);
+            $("#logo").css(cssZeroWidth); $("#menuBox").css(cssZeroWidth);
             footerOpened = false;
         }
         else {
             $("#contentBox").css(cssContentBoxNormal);
+            $("#nailHeader").css(cssNailHeaderNormal);
+            $("#logo").css(cssLogoNormal);
+            $("#menuBox").css(cssMenuBoxNormal);
             footerOpened = true;
         }
     });
